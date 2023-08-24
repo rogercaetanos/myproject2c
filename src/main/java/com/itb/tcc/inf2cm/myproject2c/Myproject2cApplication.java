f26b49640486020e66580ceffb373bffbe228ec2
Rogério Caetano <rogercaetanos@gmail.com>
2023-08-24 08:56:21 -0300

    Criação do ambiente de desenvolvimento

---
+++ src/main/java/com/itb/tcc/inf2cm/myproject2c/Myproject2cApplication.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2cm.myproject2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject2cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/tcc/inf2cm/myproject2c/Myproject2cApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.tcc.inf2cm.myproject2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
